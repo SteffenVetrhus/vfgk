6a3e4244bc6f6f530b7c4d8ca997facd5905c231
Steffen <steffenvetrhus1@gmail.com>
2020-10-03 13:59:08 +0200

    Updates

---
+++ precache-manifest.e07eb786df9b5a4ed3e8492ee0eb8167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df22c31287163980bf6cecd5001ef7e",
+    "url": "/vfgk/index.html"
+  },
+  {
+    "revision": "f3469f4e7ad173f29f33",
+    "url": "/vfgk/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b96476456e644760d9b6",
+    "url": "/vfgk/static/js/2.fd0113a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vfgk/static/js/2.fd0113a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3469f4e7ad173f29f33",
+    "url": "/vfgk/static/js/main.dd37214d.chunk.js"
+  },
+  {
+    "revision": "8f5d54bb920f6a3fc067",
+    "url": "/vfgk/static/js/runtime-main.19dc8c0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vfgk/static/media/logo.5d5d9eef.svg"
+  }
+]);
